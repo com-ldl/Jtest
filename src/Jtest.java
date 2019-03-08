dae55947e06a6895584259e7e7c0d800f51d43a0
lidonglonge <lidonglonge@163.com>
2019-03-08 15:18:17 +0800

    提交记录信息Jjava

---
+++ src/Jtest.java
@@ -0,0 +1,12 @@
+/**
+ * fileName:Jtest
+ * description:
+ * author:Ldl
+ * createTime:2019-03-08 15:08
+ */
+
+public class Jtest {
+    public static void main(String[] args) {
+        System.out.println("qwe");
+    }
+}
